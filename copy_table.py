e78233d4fd8ed07fab12ccc5d9af22926f10aa56
Christopher Motes <christopher@flux7.com>
2018-10-23 13:22:47 -0500

    pull limited string

--- copy_table.py
+++ copy_table.py
@@ -7,11 +7,12 @@ def dynamo_scan(tableName):
     client = boto3.client('dynamodb')
     try: 
         response = client.scan(
-             TableName=tableName
+             TableName=tableName,
+             Limit=1
         )
     except:
         raise
-    print response
+    print response['Items']
 
 if __name__ == "__main__":
     dynamo_scan("test0-recovery")
