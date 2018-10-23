13f06e7dac9f3069df33487037e082b684e497ca
Christopher Motes <christopher@flux7.com>
2018-10-23 16:21:00 -0500

    move to python3, add variables for source and destination table

--- copy_table.py
+++ copy_table.py
@@ -1,36 +1,39 @@
-#!/usr/bin/python
+#!/usr/bin/python3.6
 import boto3
 import botocore
+import argparse
 
-def dynamo_scan(tableName):
+def dynamo_scan(restoreTableName, destinationTableName):
     client = boto3.client('dynamodb')
-    print "Running initial data pull"
+    print("Running initial data pull")
     try: 
         response = client.scan(
-             TableName=tableName,
+             TableName=restoreTableName,
              Limit=1
         )
         itemName = response['Items'][0]
     except:
         raise
-    dynamo_put(itemName)
-    print "working through individual keys"
+    dynamo_put(itemName, destinationTableName)
+    print("working through individual keys")
     while 'LastEvaluatedKey' in response:
         try: 
+            # this print creates screen action
+            print(".", end=" ")
             response = client.scan(
-                 TableName=tableName,
+                 TableName=restoreTableName,
                  Limit=1,
                  ExclusiveStartKey=response['LastEvaluatedKey']
             )
             itemName = response['Items'][0]
         except IndexError:
-            print "\nThat was the last key! Should be recovered"
+            print("\nThat was the last key! Should be recovered")
             break
         except:
             raise
-        dynamo_put(itemName)
+        dynamo_put(itemName, destinationTableName)
 
-def dynamo_put(itemName):
+def dynamo_put(itemName, destinationTableName):
     # This section deletes the aws global table atrributes if they exist
     DeleteAttributeList = [ "aws:rep:deleting", "aws:rep:updatetime", "aws:rep:updateregion" ]
     for attribute in DeleteAttributeList:
@@ -41,12 +44,12 @@ def dynamo_put(itemName):
     client = boto3.client('dynamodb') 
     try:                              
         response = client.put_item(       
-            TableName="test0",     
+            TableName=destinationTableName,
             Item=itemName
         )                             
     except:                           
         raise                         
 
 if __name__ == "__main__":
-    dynamo_scan("test0-recovery")
+    dynamo_scan("test0-recovery", "test0")
 
